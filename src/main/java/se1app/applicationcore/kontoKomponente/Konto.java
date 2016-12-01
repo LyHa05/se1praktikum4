764f6e9b26b337fc7d01a34c849018ee157a5d67
LyHa05 <lydia.pflug@web.de>
2016-12-01 20:11:36 +0100

    Ergänzung ID bei Konto

--- src/main/java/se1app/applicationcore/kontoKomponente/Konto.java
+++ src/main/java/se1app/applicationcore/kontoKomponente/Konto.java
@@ -1,14 +1,26 @@
 package se1app.applicationcore.kontoKomponente;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import se1app.applicationcore.util.KontoNrTyp;
 
 @Entity
 public class Konto {
 
+	@Id
 	private KontoNrTyp kontoNummer;
 	
 	private int kontoStand;
+	
+//	@OneToMany(cascade = CascadeType.ALL)
+//    @JoinColumn(name="kontoNr")
+	private List<BuchungsPosition> buchungsPositionen;
 	
 	public Konto() {}
 	
