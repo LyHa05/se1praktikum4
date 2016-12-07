aa96e4e572615542ae8a6bdf5b3a8704964e3b43
LyHa05 <lydia.pflug@web.de>
2016-12-07 21:29:58 +0100

    Anpassung Application

--- src/main/java/se1app/applicationcore/Application.java
+++ src/main/java/se1app/applicationcore/Application.java
@@ -1,5 +1,8 @@
 package se1app.applicationcore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,21 +10,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import se1app.applicationcore.customercomponent.Customer;
 import se1app.applicationcore.customercomponent.CustomerRepository;
-
-import java.util.Arrays;
+import se1app.applicationcore.filialeKomponente.Filiale;
+import se1app.applicationcore.filialeKomponente.FilialeRepository;
+import se1app.applicationcore.kontoKomponente.BuchungsPosition;
+import se1app.applicationcore.kontoKomponente.Konto;
+import se1app.applicationcore.kontoKomponente.KontoKomponenteInterface;
 
 @SpringBootApplication
 public class Application {
 
+    @Autowired
+    private KontoKomponenteInterface kontoKomponenteInterface;
+	
     @Bean
-    CommandLineRunner init(CustomerRepository customerRepository) {
+    CommandLineRunner init(CustomerRepository customerRepository, FilialeRepository filialeRepository) {
         return args -> {
             Customer mickey = new Customer("Mueller");
             Customer minnie = new Customer("Meier");
             Customer pluto = new Customer("Schulze");
             customerRepository.save(Arrays.asList(mickey, minnie, pluto));
+            Filiale filiale = new Filiale();
+            filialeRepository.save(filiale);
+        	Konto k1 = new Konto("000123");
+        	kontoKomponenteInterface.kontoSpeichern(k1);
+        	BuchungsPosition bp1 = new BuchungsPosition(100);
+        	kontoKomponenteInterface.newBuchungsPosition(k1, bp1);
+    		Konto k2 = new Konto("000124");
+    		kontoKomponenteInterface.kontoSpeichern(k2);
+            
         };
     }
 
